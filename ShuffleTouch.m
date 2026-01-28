7365f25e18c1ebf7e281fc80eb1d12abac536c84
Shahaboddin <88643493+Shahaboddin@users.noreply.github.com>
2026-01-28 16:50:58 +0900

    V1.1 ShuffleTouch.m
    To be set with V1.1 userloop

--- ShuffleTouch.m
+++ ShuffleTouch.m
@@ -11,13 +11,16 @@ snd_err1.List = 'load_waveform({''sin'', .2, 200})';
 sndscene_cor1 = create_scene(snd_cor1);
 sndscene_err1 = create_scene(snd_err1);
 
-% Editable parameters
-editable('fix_window','fix_wait','fix_hold','reward','iti');
-fix_window = 6;     % acceptance radius (deg) around banana
-fix_wait   = 5000;  % ms to start touching
-fix_hold   = 50;    % ms to hold
-reward     = 150;   % solenoid open (ms)
-iti        = 1500;  % ms
+% Editable timing parameters only [web:15]
+editable('fix_window','fix_wait','fix_hold','reward','iti', ...
+         'max_trials_edit');
+
+fix_window      = 4.5;      % deg
+fix_wait        = 5000;   % ms
+fix_hold        = 50;     % ms
+reward          = 150;    % ms
+iti             = 2500;   % ms
+max_trials_edit = 500;
 
 % Event codes
 BUTTON = 10;
@@ -25,32 +28,36 @@ REWARD = 90;
 bhv_code(BUTTON,'Button',REWARD,'Reward');
 
 % Scene: touch & hold banana (TaskObject #1)
-fix1 = SingleTarget(tracker);    % adapter
-fix1.Target    = 1;              % TaskObject #1 is banana
-fix1.Threshold = fix_window;     % circular window radius [web:16][web:22]
+fix1 = SingleTarget(tracker);
+fix1.Target    = 1;
+fix1.Threshold = fix_window;
 
-wth1 = WaitThenHold(fix1);       % adapter
-wth1.WaitTime = fix_wait;        % ms to acquire
-wth1.HoldTime = fix_hold;        % ms to hold
+wth1 = FreeThenHold(fix1);
+wth1.WaitTime = fix_wait;
+wth1.HoldTime = fix_hold;
 
-scene1 = create_scene(wth1, 1);  % draw TaskObject #1 [web:22]
+scene1 = create_scene(wth1, 1);
 
 % Run trial
 error_type = 0;
-rt_touch = NaN;
+rt_touch   = NaN;
 
-idle(500);                       % blank
+idle(500);
+run_scene(scene1, BUTTON);
 
-run_scene(scene1, BUTTON);       % show banana, run adapters
-rt_touch = wth1.AcquiredTime;    % time to first touch
-rt = rt_touch;    % ML uses "rt" for the default reaction time display [web:89]
+rt_touch = wth1.RT;
+rt       = rt_touch;
 
-
-if ~wth1.Success
-    error_type = 3;              % treat as touch break / no touch
+if wth1.Success
+    error_type = 0;
+else
+    if isnan(rt_touch) || rt_touch <= 0
+        error_type = 1;
+    else
+        error_type = 3;
+    end
 end
 
-% Reward / error
 if error_type == 0
     run_scene(sndscene_cor1);
     goodmonkey(reward, 'numreward', 1, 'eventmarker', REWARD);
@@ -60,8 +67,7 @@ end
 
 idle(iti);
 
-% Log
 trialerror(error_type);
-bhv_variable('button_loc', TrialRecord.User.button_loc);  % [x y]
+bhv_variable('button_loc', TrialRecord.User.button_loc);
 bhv_variable('touch_rt',   rt_touch);
 bhv_variable('reward_ms',  reward);
